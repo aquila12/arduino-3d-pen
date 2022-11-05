4410cbdb04de0fcb802dfcc4a09ae2b0b83ff0d9
Nick Moriarty <njmoriarty@gmail.com>
2022-11-05 23:49:45 +0000

    First properly working version
    Set up temperature PID loop
    Set up status LEDs
    Fix some bodges

--- pen_control.ino
+++ pen_control.ino
@@ -10,6 +10,9 @@
 #define IDLE_DELAY 25
 #define AUTOREPEAT_DELAY 500
 #define AUTOREPEAT_EVERY 50
+#define LED_FLASH_DELAY (led_flash ? 667 : 333)
+#define OVERTEMP_LIMIT 15
+#define PID_THRESHOLD 5
 
 /*** INPUT FUNCTIONS ***/
 
@@ -56,9 +59,20 @@ void resetOutputs() {
   setHeater(0); 
 }
 
-void setLEDGreen() { pinMode(LED_READY, OUTPUT); digitalWrite(LED_READY, 1); };
-void setLEDRed() { pinMode(LED_READY, OUTPUT); digitalWrite(LED_READY, 0); };
-void setLEDOff() { pinMode(LED_READY, INPUT); };
+enum led_state { LED_OFF, LED_G_ON, LED_R_ON, LED_G_FLASH, LED_R_FLASH };
+void setLED(led_state state, bool flash_state = true) {
+  if(!flash_state && (state == LED_G_FLASH || state == LED_R_FLASH)) state = LED_OFF;
+  
+  switch(state) {
+    case LED_OFF: pinMode(LED_READY, INPUT); break;
+    case LED_G_ON:
+    case LED_G_FLASH: pinMode(LED_READY, OUTPUT); digitalWrite(LED_READY, 1); break;
+    case LED_R_ON:
+    case LED_R_FLASH: pinMode(LED_READY, OUTPUT); digitalWrite(LED_READY, 0); break;
+  }
+}
+
+inline int clamp(int x, int x_min, int x_max) { return (x < x_min) ? x_min : ((x > x_max) ? x_max : x); };
 
 /*** PROGRAM ***/
 
@@ -78,9 +92,9 @@ void yield(int ms) {
 
 void doSplash() {
   setLCDString("3D"); yield(1000);
-  setLEDRed(); yield(500);
-  setLEDGreen(); yield(500);
-  setLEDOff(); yield(1000);
+  setLED(LED_R_ON); yield(500);
+  setLED(LED_G_ON); yield(500);
+  setLED(LED_OFF); yield(1000);
 }
 
 void setup() {
@@ -94,10 +108,16 @@ void setup() {
 
 void loop() {  
   static time_ms display_revert_time = 0;
+  static time_ms led_flash_time = 0;
+  static time_ms motor_reverse_time = 0;
+  static bool led_flash = true;
+  static led_state led = LED_OFF;
   static bool enable_heater = false;
   static bool enable_motor = false;
 
   yield(AUTOREPEAT_EVERY);
+
+  time_ms now = current_time();
 
   if(key) {
     // "Key presses"
@@ -105,12 +125,12 @@ void loop() {
       if(key == K_INC || key == K_DEC) {
         temperature_setpoint += (key == K_INC) ? 1 : -1;
         setLCDNumeric(temperature_setpoint);
-        display_revert_time = current_time() + 2000;
+        display_revert_time = now + 2000;
       }
     }
 
     if(key == K_FWD) { enable_heater = true; }
-    if(key == K_REV && keydown_ms() > 1000) { enable_heater = false; } // Bodge
+    if(key == K_REV && keydown_ms() > 1000) { enable_heater = false; }
   }
 
   key_changed = false;
@@ -118,20 +138,56 @@ void loop() {
   // TODO: Implement a PID loop that does the job when switching between extrude / not
   if(enable_heater) {
     int dt = temperature - temperature_setpoint;
-    if(dt > 15) enable_heater = false;
-    else if(dt > 0) setHeater(0);
-    else if(dt > -7) setHeater(255 - (dt + 7) << 5);
-    else setHeater(255);
+    static int dt_last = dt;
+    static int i = 0;
+    int p, d;
 
-    enable_motor = (dt > -5);
-  } else enable_motor = false;
+    if(dt > OVERTEMP_LIMIT) {
+      // Trip to prevent thermal runaway
+      enable_heater = false;
+    } else if(dt > PID_THRESHOLD) {
+      setHeater(0);
+      enable_motor = true;
+      led = LED_R_FLASH;
+    } else if(dt < -PID_THRESHOLD) {
+      setHeater(255);
+      enable_motor = false;
+      led = LED_R_ON;
+    } else {
+      // PID Control
+      p = dt;
+      i = clamp(i + dt, -511, 511);
+      d = dt - dt_last;
+
+      int sum = (256 * p) + (4 * i) + (8192 * d);
+      int htr = clamp(256 - (sum >> 2), 0, 255);
+      setHeater(htr);
+      // setLCDNumeric(htr); display_revert_time = now + 1000;
+      enable_motor = true;
+      led = LED_G_ON;
+    }
+    
+    dt_last = dt;
+  }
   
-  if(!enable_heater) setHeater(0);
+  if(!enable_heater) {
+    setHeater(0);
+    led = (temperature < 50) ? LED_G_FLASH : LED_OFF;
+  }
+  
   if(enable_motor) {
     if(key == K_FWD) setMotorForwards(speed);
-    else if(key == K_REV) { setMotorReverse(); yield(1000); } // Bodge; it will disable heater
+    else if(now < motor_reverse_time) setMotorReverse();
+    else if(key == K_REV) { setMotorReverse(); motor_reverse_time = now + 1000; }
     else setMotorForwards(0);
   } else setMotorForwards(0);
 
-  if(current_time() > display_revert_time) setLCDNumeric(temperature);
+  if(now > display_revert_time) setLCDNumeric(temperature);
+  
+  if(now > led_flash_time) {
+    led_flash = !led_flash;
+    led_flash_time = now + LED_FLASH_DELAY;
+  }
+
+  setLED(led, led_flash);
 }
--- temperature.h
+++ temperature.h
@@ -2,7 +2,7 @@
 #define MAX_TEMP_READING 999
 #define UNINITIALIZED_TEMP_READING -100
 
-#define DEFAULT_TEMP_SETPOINT 100
+#define DEFAULT_TEMP_SETPOINT 180
 
 extern int temperature;  //°C
 extern int temperature_setpoint;
